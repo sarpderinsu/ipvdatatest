e88057a622470889e772dcfa9e9f33722f454620
Sarp <sarp@thehuddle.nl>
2023-04-17 11:10:31 +0200

    WIP

--- resources/js/pages/TaxonomyViewPage.vue
+++ resources/js/pages/TaxonomyViewPage.vue
@@ -9,12 +9,57 @@
                 <th class="text-left">
                     Image
                 </th>
+                <th class="text-left">
+                    Link
+                </th>
+                <th class="text-left">
+                    Brand
+                </th>
+                <th class="text-left">
+                    Category
+                </th>
+                <th class="text-left">
+                    Size
+                </th>
+                <th class="text-left">
+                    Price
+                </th>
+                <th class="text-left">
+                    Discount
+                </th>
             </tr>
             </thead>
             <tbody>
             <tr v-for="item in products" :key="item.id">
                 <td>{{ item.title }}</td>
                 <td><v-img :src="item.image"></v-img></td>
+                <td><v-btn color="blue" :href="'https://ah.nl/' + item.link">ah.nl</v-btn></td>
+                <td>{{ item.brand }}</td>
+                <td>{{ item.category }}</td>
+                <td>{{ item.price.unitSize }}</td>
+                <td>
+                    <v-chip
+                        v-if="item.price.was"
+                        class="ma-2">
+                        <s>{{ Number(item.price.was).toFixed(2) }}</s>
+                    </v-chip>
+                    <v-chip
+                        :color="item.price.theme === 'bonus' ? 'orange' : 'blue'"
+                        class="ma-2">
+                        {{ Number(item.price.now).toFixed(2) }}
+                    </v-chip>
+                    <v-chip
+                        v-if="item.price.unitInfo"
+                        class="ma-2"
+                        color="green"
+                        text-color="white">
+                        {{ Number(item.price.unitInfo.price).toFixed(2) }} / {{ item.price.unitInfo.description }}
+                    </v-chip>
+                </td>
+                <td>
+                    <v-chip v-if="item.discount.bonusType" color="orange">{{ item.discount.bonusType }}</v-chip>
+                    <v-chip v-if="item.discount.promotionType" color="blue">{{ item.discount.promotionType }}</v-chip>
+                </td>
             </tr>
             </tbody>
         </v-table>
@@ -24,7 +69,7 @@
 <script>
 import {useQuery} from "@vue/apollo-composable";
 import {taxonomyQuery} from "../../gql/ah";
-import {computed, watch} from "vue";
+import {computed} from "vue";
 import {useRoute} from "vue-router";
 
 export default {
