ff4ee2d152cdcc08e7a3b06f9944b88f9a1adf69
Sarp <sarp@thehuddle.nl>
2023-04-17 13:20:11 +0200

    WIP

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,61 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        Product::create([
-            'name' => 'heh',
-            'taxonomy_slug' => 'asd'
-        ]);
-
-        Product::create([
-            'name' => 'heh',
-            'taxonomy_slug' => 'asd'
-        ]);
-
-        Product::create([
-            'name' => 'heh',
-            'taxonomy_slug' => 'asd'
-        ]);
-
-        Product::create([
-            'name' => 'heh',
-            'taxonomy_slug' => 'fgaag'
-        ]);
-
-        Product::create([
-            'name' => 'heh',
-            'taxonomy_slug' => 'fgaag'
-        ]);
-
-        Product::create([
-            'name' => 'heh',
-            'taxonomy_slug' => 'fgaag'
-        ]);
-
-        Product::create([
-            'name' => 'heh',
-            'taxonomy_slug' => 'mefghhfdh'
-        ]);
-
-        Product::create([
-            'name' => 'heh',
-            'taxonomy_slug' => 'dfjdfj'
-        ]);
-
-        Product::create([
-            'name' => 'heh',
-            'taxonomy_slug' => 'dfjdfj'
-        ]);
-
-        Product::create([
-            'name' => 'heh',
-            'taxonomy_slug' => 'dfjdfj'
-        ]);
+        //
     }
 }
